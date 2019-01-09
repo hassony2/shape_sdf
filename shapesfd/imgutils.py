a7b663b8af83740a13a0fb8505bfb63258e81fcf
Yana <yanahasson@gmail.com>
2019-01-09 17:33:19 +0100

    Add colorbars

--- shapesfd/imgutils.py
+++ shapesfd/imgutils.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 
-def plot_sdf(grid_vals, gt=None, grid_step=1):
+def plot_sdf(grid_vals, gt=None, grid_step=1, cmap='seismic'):
     plot_nb = int(grid_vals.shape[0] / grid_step)
 
     if gt is None:
@@ -9,10 +9,12 @@ def plot_sdf(grid_vals, gt=None, grid_step=1):
         row_nb = 2
     for plot_idx in range(0, plot_nb):
         plt.subplot(row_nb, plot_nb, plot_idx + 1)
-        plt.imshow(grid_vals[grid_step * plot_idx], vmin=-2, vmax=1)
+        ax = plt.imshow(grid_vals[grid_step * plot_idx], vmin=-2, vmax=1, cmap=cmap)
+        plt.colorbar(ax)
 
     if gt is not None:
         for plot_idx in range(0, plot_nb):
             plt.subplot(row_nb, plot_nb, plot_nb + plot_idx + 1)
-            plt.imshow(gt[grid_step * plot_idx], vmin=-2, vmax=1)
+            ax = plt.imshow(gt[grid_step * plot_idx], vmin=-2, vmax=1, cmap=cmap)
+            plt.colorbar(ax)
 
